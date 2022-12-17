fa206250ad8f3908c7bada66a0df8d22b2bf2607
Ryu Pradana <ryuprad@gmail.com>
2022-12-17 12:03:11 -0500

    CardDeal section, finished

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { card } from '../assets';
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
+const CardDeal = () =>  (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better deal <br className='sm:block hidden'/>
+        in few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        I am focusing on becoming a full stack developer,
+        my prefer tech stack is the MERN stack, I want to develop 
+        website and mobile application.
+      </p>
+
+      <Button styles="mt-10"/>
+    </div>  
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className='w-[100%] h-[100%]'/>
+
+    </div>
+  </section>
   )
-}
 
 export default CardDeal
