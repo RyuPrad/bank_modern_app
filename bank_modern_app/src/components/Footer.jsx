f17772ac2e04248f8f03138b7e7a436369f4ef48
Ryu Pradana <ryuprad@gmail.com>
2022-12-17 21:25:48 -0500

    Fix Navbar

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import styles from "../style";
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () =>  (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div>
+
+    </div>
+  </section>
+)
+
 
 export default Footer
--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             className={`font-poppins font-normal cursor-pointer text-[16px]` +
                         ` ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
           >
-            <a href={'#${nav.id}'}>
+            <a href={`#${nav.id}`}>
               {nav.title}
             </a>
           </li>
